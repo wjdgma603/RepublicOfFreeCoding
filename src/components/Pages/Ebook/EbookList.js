104b46362b5d071bc7fb3083f2dbe4f237dd7354
wjdgma603 <144869990+wjdgma603@users.noreply.github.com>
2023-11-24 17:16:58 +0900

    Ebook 기능 구현 100%

--- src/components/Pages/Ebook/EbookList.js
+++ src/components/Pages/Ebook/EbookList.js
@@ -6,13 +6,27 @@ import { Link } from 'react-router-dom';
 import './Ebook.css'
 
 const EbookList = () => {
-    const [ TabMenu, setTabMenu ] = useState("전체")
+    const [ TabMenu, setTabMenu ] = useState("전체");
+    const [ sortList, setSortList] = useState(EbookItem);
+    const getSortList = ()=>{
+        const sortcallBack = (item)=>{
+            if(TabMenu === "Html"){
+                return item.Category === "Html"
+            }else if(TabMenu === "Css"){
+                return item.Category === "Css"
+            }else if(TabMenu === "Javascript"){
+                return item.Category === "Javascript"
+            }        
+        }
+        const SortList = TabMenu === '전체' ? sortList : sortList.filter((item)=>sortcallBack(item))
+        return SortList;
+    }
     return ( 
         <section className="EbookSection">
             <DepthMenu setTabMenu={setTabMenu} Data={EbookTab} FilterName={'필터'}/>
             <article className='EbookList'>
-                <div className='EbookTotalNum'>전체 <span className='Green'>{EbookItem.length}</span>개</div>
-                {EbookItem.map((Ebook)=>
+                <div className='EbookTotalNum'>전체 <span className='Green'>{getSortList().length}</span>개</div>
+                {getSortList().map((Ebook)=>
                     <div className='EbookItem' key={Ebook.id}>
                         <div className='ImgWrap'>
                             <Link to={`/ebook/${Ebook.id}`} state={{Ebook : Ebook}}>
