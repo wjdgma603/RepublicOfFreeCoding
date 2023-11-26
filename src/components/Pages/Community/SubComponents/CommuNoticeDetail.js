958cc6f9816f61944fbf82f08993cbeb5b6c9aeb
sungtae <stst9312@gmail.com>
2023-11-26 15:23:53 +0900

    ..

--- src/components/Pages/Community/SubComponents/CommuNoticeDetail.js
+++ src/components/Pages/Community/SubComponents/CommuNoticeDetail.js
@@ -23,14 +23,6 @@ const CommuNoticeDetail = () => {
     }
   }, [id]);
 
-  useEffect(() => {
-    const savedData = JSON.parse(localStorage.getItem(`editedPost_${id}`));
-    if (savedData) {
-      setEditedTitle(savedData.editedTitle);
-      setEditedContent(savedData.editedContent);
-    }
-  }, []);
-
   const handleEditClick = () => {
     setIsEditing(true);
   };
@@ -59,10 +51,22 @@ const CommuNoticeDetail = () => {
   };
 
   const deletePost = () => {
-    // ... (이전 코드 생략)
+    const confirmation = window.confirm("정말로 게시글을 삭제하시겠습니까?");
+    if (confirmation) {
+      // noticePost에서 해당 게시글 삭제
+      const updatedNoticePosts = noticePosts?.postIndex?.filter((post) => post.id !== parseInt(id));
+      const updatedNoticePostsObject = {
+        ...noticePosts,
+        postIndex: updatedNoticePosts,
+      };
+      setNoticePosts(updatedNoticePostsObject);
+      localStorage.setItem('noticePosts', JSON.stringify(updatedNoticePostsObject));
 
-    localStorage.removeItem(`editedPost_${id}`);
-    navigate('/community');
+      // editedPost에서 해당 게시글 삭제
+      localStorage.removeItem(`editedPost_${id}`);
+
+      navigate('/community');
+    }
   };
 
   const goBack = () => {
@@ -149,3 +153,158 @@ const CommuNoticeDetail = () => {
 };
 
 export default CommuNoticeDetail;
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { Link, useParams, useNavigate } from 'react-router-dom';
+// import { noticePost, updateNoticePost } from './CommuData';
+// import './CommuNoticeDetail.css';
+
+// const CommuNoticeDetail = () => {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const [noticePosts, setNoticePosts] = useState(noticePost);
+//   const [isEditing, setIsEditing] = useState(false);
+//   const selectedPost = noticePosts?.postIndex?.find((post) => post.id === parseInt(id));
+//   const [editedTitle, setEditedTitle] = useState(selectedPost?.title || '');
+//   const [editedContent, setEditedContent] = useState(selectedPost?.content || '');
+
+//   useEffect(() => {
+//     const storedData = JSON.parse(localStorage.getItem('noticePosts')) || noticePost;
+//     setNoticePosts(storedData);
+
+//     const savedData = JSON.parse(localStorage.getItem(`editedPost_${id}`));
+//     if (savedData) {
+//       setEditedTitle(savedData.editedTitle);
+//       setEditedContent(savedData.editedContent);
+//     }
+//   }, [id]);
+
+//   useEffect(() => {
+//     const savedData = JSON.parse(localStorage.getItem(`editedPost_${id}`));
+//     if (savedData) {
+//       setEditedTitle(savedData.editedTitle);
+//       setEditedContent(savedData.editedContent);
+//     }
+//   }, []);
+
+//   const handleEditClick = () => {
+//     setIsEditing(true);
+//   };
+
+//   const handleSaveClick = () => {
+//     setNoticePosts((prevNoticePosts) => {
+//       const updatedPosts = prevNoticePosts?.postIndex?.map((post) => {
+//         if (post.id === parseInt(id)) {
+//           return { ...post, title: editedTitle, content: editedContent };
+//         } else {
+//           return post;
+//         }
+//       });
+
+//       return { ...prevNoticePosts, postIndex: updatedPosts };
+//     });
+
+//     updateNoticePost(parseInt(id), editedTitle, editedContent);
+//     setIsEditing(false);
+
+//     localStorage.setItem(`editedPost_${id}`, JSON.stringify({ editedTitle, editedContent }));
+//   };
+
+//   const handleCancelClick = () => {
+//     setIsEditing(false);
+//   };
+
+//   const deletePost = () => {
+//     // ... (이전 코드 생략)
+
+//     localStorage.removeItem(`editedPost_${id}`);
+//     navigate('/community');
+//   };
+
+//   const goBack = () => {
+//     navigate(-1);
+//   };
+
+//   return (
+//     <div className="CommuSection">
+//       <div>
+//         <div>
+//           <div>커뮤니티</div>
+//           <div>초기화</div>
+//         </div>
+//         <ul className="CommuNav">
+//           <li>
+//             <Link to="/community">공지사항</Link>
+//           </li>
+//           <li>
+//             <Link to="/community/qna">문의사항</Link>
+//           </li>
+//           <li>
+//             <Link to="/community/faq">FAQ</Link>
+//           </li>
+//         </ul>
+//       </div>
+
+//     <div className="CommuRight">
+//         <h1>공지사항</h1>
+
+//         {isEditing ? (
+//           <div>
+//             <input
+//               className="CommuQnaEditTitle"
+//               type="text"
+//               value={editedTitle}
+//               onChange={(e) => setEditedTitle(e.target.value)}
+//             />
+//           </div>
+//         ) : (
+//           <div className="CommuQnaTitle">
+//             <div>{editedTitle}</div>
+//             <div></div>
+//           </div>
+//         )}
+
+//         {isEditing ? (
+//           <textarea
+//             className="CommuQnaEditText"
+//             value={editedContent}
+//             onChange={(e) => setEditedContent(e.target.value)}
+//           />
+//         ) : (
+//           <div className="CommuQnaText">{editedContent}</div>
+//         )}
+
+//         {isEditing ? (
+//           <div>
+//             <div className="CommuQnaEditDetailButtonWrap">
+//               <button className="CommuQnaEditDetailButton" onClick={handleSaveClick}>
+//                 저장
+//               </button>
+//               <button className="CommuQnaEditDetailButton" onClick={handleCancelClick}>
+//                 취소
+//               </button>
+//             </div>
+//           </div>
+//         ) : (
+//           <div className="CommuNoticeDetailButtonWrap">
+//             <div></div>
+           
+//               <button className="CommuNoticePageButton" onClick={goBack}>목록보기</button>
+
+//             <div>
+//               <button className="CommuNoticeEditButton" onClick={handleEditClick}>
+//                 글수정
+//               </button>
+//               <button className="CommuDetailDeleteButton" onClick={deletePost}>게시글 삭제</button>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CommuNoticeDetail;
